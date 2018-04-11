187f79649bd08326a196818adda1f9b35f3c09db
coder <coder@YARE.local>
2018-04-12 00:08:11 +0800

    appledaily news for appnews finished

--- appNews/views.py
+++ appNews/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+
+import requests
 
 # Create your views here.
 
 def home(request):
+    news_list = []
 
-    return render(request, 'appNews/home.html')
+    url_Apple = "https://tw.appledaily.com/recommend/realtime"
+    res_Apple = requests.get(url_Apple)
+    soup_Apple = BeautifulSoup(res_Apple.text, "html.parser")
+    news_Apple = soup_Apple.select(".item")
+    for news in news_Apple:
+        # new = (title, url, img, source)
+        new = (news.find("img").get("alt"), news.find("a").get("href"), news.find("img").get("data-src"), "蘋果日報")
+        news_list.append(new)
+
+    return render(request, 'appNews/home.html', locals())
