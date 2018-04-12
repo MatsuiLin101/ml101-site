47605d68eecc2235010fef7c340ec769a949ecb5
coder <coder@YARE.local>
2018-04-12 23:45:44 +0800

    create base.html for other html navbar template

--- appHomePage/views.py
+++ appHomePage/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    previous = ""
 
-    return render(request, 'appHomePage/home.html')
+    return render(request, 'appHomePage/home.html', locals())
